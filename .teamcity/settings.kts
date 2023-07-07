9f52406e0b99c215b239580be8b24927614a6e0c
dale.marttinen@clear-measure.com <dale.marttinen@clear-measure.com@TeamCity>
2023-07-07 16:31:20 +0000

    TeamCity change in 'Onion Architecture Dotnet 7 Container Apps' project: runners of 'TDD' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -236,6 +236,10 @@ object Tdd : BuildType({
             name = "Get Container App URL"
             scriptMode = script {
                 content = """
+                    Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
+                    Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+                    Remove-Item .\AzureCLI.msi
+                    
                     az config set extension.use_dynamic_install=yes_without_prompt
                     # Log in to Azure
                     az login --service-principal --username %AzAppId% --password %AzPassword% --tenant %AzTenant%
