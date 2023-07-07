023dc54e2a695c549bc54477a4f3226c5b69c4e3
dale.marttinen@clear-measure.com <dale.marttinen@clear-measure.com@TeamCity>
2023-07-07 16:19:11 +0000

    TeamCity change in 'Onion Architecture Dotnet 7 Container Apps' project: runners of 'TDD' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -241,8 +241,10 @@ object Tdd : BuildType({
                     az login --service-principal --username %AzAppId% --password %AzPassword% --tenant %AzTenant%
                     ${'$'}containerAppURL = az containerapp show --resource-group %TDD-Resource-Group%-3.0.100 --name %TDD-App-Name% --query properties.configuration.ingress.fqdn
                     ${'$'}containerAppURL = ${'$'}containerAppURL -replace '"', ''
-                    [System.Environment]::SetEnvironmentVariable("containerAppURL", ${'$'}containerAppURL, "Machine")
-                    Write-Host "##teamcity[setParameter name='env.containerAppURL' value=' ${'$'}{'${'$'}'}containerAppURL']"
+                    Write-Host "url retrieved from AZ: ${'$'}containerAppURL"
+                    #[System.Environment]::SetEnvironmentVariable("containerAppURL", ${'$'}containerAppURL, "Machine")
+                    Write-Host "##teamcity[setParameter name='env.containerAppURL' value='${'$'}containerAppURL']"
+                    Write-Host "ContainerAppURl after retrieval: ${'$'}env:containerAppURL"
                 """.trimIndent()
             }
         }
