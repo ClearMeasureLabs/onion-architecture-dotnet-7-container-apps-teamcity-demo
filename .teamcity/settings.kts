61d69c60968f419b270032a3476146bc61f0e46a
dale.marttinen@clear-measure.com <dale.marttinen@clear-measure.com@TeamCity>
2023-07-07 19:31:07 +0000

    TeamCity change in 'Onion Architecture Dotnet 7 Container Apps' project: runners of 'Prod' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -273,6 +273,7 @@ object Prod : BuildType({
             name = "Deploy To Prod"
             type = "octopus.deploy.release"
             param("octopus_space_name", "%OctoSpaceName%")
+            param("octopus_waitfordeployments", "true")
             param("octopus_version", "3.0+")
             param("octopus_host", "%OctoURL%")
             param("octopus_project_name", "%OctoProject%")
